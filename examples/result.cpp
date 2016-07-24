9da8be27bcf6f06582c0754e69501987f1ffb8e8
underrated <johndoe@underrated.org>
2016-07-24 15:01:33 +0100

    * fixed #endif issue * Separated ScoreListener in separate file

--- examples/result.cpp
+++ examples/result.cpp
@@ -1,8 +1,12 @@
- example::example() {} example::~example() {}void example::set_x(int ox) {
+ example::example() {}
+ example::~example() {}
+void example::set_x(int ox) {
     x = ox;
-  }int example::get_x() {
+  }
+int example::get_x() {
     return x;
-  }int main_dummy() {
+  }
+int main_dummy() {
   example* ex_inst = new example();
   ex_inst.set_x(5);
   caout << ex_inst.get_x() << endl;
--- examples/result.hpp
+++ examples/result.hpp
@@ -1,4 +1,12 @@
-#include <iostream>#include <systemc>#include <vector>#include <string>#ifndef __example__cpp#define __example__cppusing namespace std;int global;template <typename T = int>
+#include <iostream>
+#include <systemc>
+#include <vector>
+#include <string>
+#ifndef __example__cpp
+#define __example__cpp
+using namespace std;
+int global;
+template <typename T = int>
 class example_tpl {
   T tpl_field;
 
@@ -9,7 +17,29 @@ class example_tpl {
   void set_field(T field) {
     this.tpl_field = field;
   }
-};#define THIStemplate<typename T>
+};
+#define THIS
+template<typename T>
 T funct(int x) {
   return x + 1;
-}class example {string field; int x = 1; int *y = NULL; vector<string> names; void *(*foo)(int *);  example(); ~example();void set_x(int ox);int get_x();float prototype(int arg); };class example1 {};int f(int);class example2 : public example, example1 {};int standalone_prototype(int z);int main_dummy();
+}
+class example {
+string field; 
+int x = 1; 
+int *y = NULL; 
+vector<string> names; 
+void *(*foo)(int *); 
+ example();
+ ~example();
+void set_x(int ox);
+int get_x();
+float prototype(int arg); 
+};
+class example1 {
+};
+int f(int);
+class example2 : public example, example1 {
+};
+int standalone_prototype(int z);
+int main_dummy();
+#endif
